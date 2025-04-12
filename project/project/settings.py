c00c452aa67c1d7ad914f141731b8f839ed6f541
MarkosHB <marcoshidalgobanos@gmail.com>
2025-04-12 16:29:23 +0200

    Update for dotenv integration

--- project/project/settings.py
+++ project/project/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4*!+qu^ql0i@6v^&00-a8__#5ntr(#72h+x7+nva9ck*@as(-m'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.Appconfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
