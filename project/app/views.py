6eca65e88376dd6292b29194f4ad64aac2db7561
MarkosHB <marcoshidalgobanos@gmail.com>
2025-04-13 14:32:46 +0200

    feature: Basic login page and redirect to homepage

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the app index.")
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("homepage")
+        else:
+            messages.error(request, "Invalid username or password")
+            
+    return render(request, "login.html")
+
+
+def homepage(request):
+    return HttpResponse("Hello, world. You're at the app homepage.")
